62c08681d54913f07ee95f1c04d9ad60baa32623
Anthony Nguyen <anthony3@ualberta.ca>
2022-09-27 15:24:52 -0600

    Updated license

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2022 Anthony Nguyen
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
